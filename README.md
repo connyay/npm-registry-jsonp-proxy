b61db0022f3a4a3ae57152ab06d8aae3d1d24c5e
Connor Hindley <conn.hindley@gmail.com>
2014-09-09 20:25:43 -0400

    initial proxy work

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 npm-registry-jsonp-proxy
 ========================
+
+Simple jsonp proxy to assist [https://github.com/anvaka/npmgraph.an](anvaka/npmgraph.an)
+
+Basically just a stripped down version of [https://github.com/clintandrewhall/node-jsonp-proxy](clintandrewhall/node-jsonp-proxy)
---
+++ proxy.js
@@ -0,0 +1,53 @@
+// Stripped down version of - A Simple JSONP Proxy for NodeJS by - http://twitter.com/oJshua
+'use strict';
+
+var http = require('http'),
+    url = require('url'),
+    request = require('request');
+
+var port = parseInt(process.env.PORT) || 8001;
+
+http.createServer(function(req, res) {
+    var params = url.parse(req.url, true).query;
+    function sendUsage() {
+        res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'text/plain'
+        });
+        res.write('npm registry jsonp proxy\n\n');
+        res.write('Usage:\n');
+        res.write('\tpath: The path to access, (required).\n');
+        res.write('\tcallback: The function name to use for the JSON response. Default is "callback".\n');
+        return res.end();
+    }
+    function sendFailBoat() {
+        res.writeHead(400, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'text/plain'
+        });
+        res.write('Whoops. Something failed');
+        return res.end();
+    }
+    function sendJSONP(body) {
+        var callback = params.callback || 'callback';
+        res.writeHead(200, {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/javascript'
+        });
+        res.write(callback + '(' + body + ')');
+        return res.end();
+    }
+    if (!params.path) {
+        sendUsage();
+    } else {
+        request('http://registry.npmjs.org/' + params.path, function(err, response, body) {
+            if (!err) {
+                sendJSONP(body);
+            } else {
+                sendFailBoat();
+            }
+        });
+    }
+}).listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port);
